d4fb25955650a10334da943b6ab0590af82a4053
Dan Bucholtz <dan.bucholtz@gmail.com>
2018-05-18 01:04:43 -0500

    progress

--- src/components/header/th-nav/th-nav.tsx
+++ src/components/header/th-nav/th-nav.tsx
@@ -13,15 +13,17 @@ export class ThNav {
         <a href="/">
           <div class="logo">
             <img src="/assets/images/logo.png"></img>
-            <h1>Theracode</h1>
+            <h1 class="title">Theracode</h1>
           </div>
         </a>
         <nav>
-          <a href="/about">About</a>
+          <stencil-route-link url="/about">About</stencil-route-link>
           <a href="https://medium.com/@codetherapy">Blog</a>
-          <a href="/solutions">Solutions</a>
-          <a href="/culture">Culture</a>
-          <a href="/contact"><ion-button>Contact</ion-button></a>
+          <stencil-route-link url="/solutions">Solutions</stencil-route-link>
+          <stencil-route-link url="/culture">Culture</stencil-route-link>
+          <stencil-route-link url="/contact">
+            <ion-button>Contact</ion-button>
+          </stencil-route-link>
         </nav>
         <div class="menu-icon">
           <ion-menu-button></ion-menu-button>
