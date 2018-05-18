5cd7a9e31d0fed0efb2e1c81676666ea0873daca
Dan Bucholtz <dan.bucholtz@gmail.com>
2018-05-18 00:34:49 -0500

    form validation

--- src/components/contact-page/th-contact-body-content/th-contact-body-content.tsx
+++ src/components/contact-page/th-contact-body-content/th-contact-body-content.tsx
@@ -10,27 +10,36 @@ export class ThContactBodyContent {
   @State() name = "";
   @State() email = "";
   @State() message = "";
+
+  submitForm(event: Event) {
+    event.preventDefault();
+    console.log('Submit this bizzle here');
+  }
   
   render() {
     const isActive = !!(this.name.length && this.email.length && this.message.length);
     return [
       <div class="container">
         <h1>Let's chat</h1>
-        <form>
+        <form onSubmit={this.submitForm.bind(this)}>
           <div class="input-group">
             <h2>Name<span class="required">  *</span></h2>
             <input type="text" placeholder="Your Name" class="form-input" onInput={(event: any) => this.name = event.target.value}/>
           </div>
           <div class="input-group">
             <h2>Email Address<span class="required">  *</span></h2>
-            <input type="text" placeholder="Your Email Address" class="form-input" onInput={(event: any) => this.email = event.target.value}/>
+            <input type="email" placeholder="Your Email Address" class="form-input" onInput={(event: any) => this.email = event.target.value}/>
           </div>
           <div class="input-group">
             <h2>Message<span class="required">  *</span></h2>
-            <textarea placeholder="Message" class="form-input" onInput={(event: any) => this.message = event.target.value}/>
+            <textarea 
+              placeholder="Message"
+              class="form-input"
+              minlength="10"
+              onInput={(event: any) => this.message = event.target.value}/>
           </div>
           <div class="input-group">
-            <ion-button expand="block" disabled={!isActive}>Contact Us</ion-button>
+            <ion-button type="submit" expand="block" disabled={!isActive}>Contact Us</ion-button>
           </div>
         </form>
       </div>
