3784744212101ce8f68c1aa14555756cb8cac1a6
Dan Bucholtz <dan.bucholtz@gmail.com>
2018-05-21 01:19:05 -0500

    remove firebase functions, just use formspring for now

--- src/components/contact-page/th-contact-body-content/th-contact-body-content.tsx
+++ src/components/contact-page/th-contact-body-content/th-contact-body-content.tsx
@@ -1,4 +1,4 @@
-import { Component, State } from '@stencil/core';
+import { Component } from '@stencil/core';
 
 @Component({
   tag: 'th-contact-body-content',
@@ -7,39 +7,32 @@ import { Component, State } from '@stencil/core';
 })
 export class ThContactBodyContent {
 
-  @State() name = "";
-  @State() email = "";
-  @State() message = "";
 
-  submitForm(event: Event) {
-    event.preventDefault();
-    console.log('Submit this bizzle here');
-  }
-  
   render() {
-    const isActive = !!(this.name.length && this.email.length && this.message.length);
     return [
       <div class="container">
         <h1>Let's chat</h1>
-        <form onSubmit={this.submitForm.bind(this)}>
+        <form action="https://formspree.io/dan.bucholtz@gmail.com" method="POST">
           <div class="input-group">
             <h2>Name<span class="required">  *</span></h2>
-            <input type="text" placeholder="Your Name" class="form-input" onInput={(event: any) => this.name = event.target.value}/>
+            <input name="name" type="text" placeholder="Your Name" class="form-input" required/>
           </div>
           <div class="input-group">
             <h2>Email Address<span class="required">  *</span></h2>
-            <input type="email" placeholder="Your Email Address" class="form-input" onInput={(event: any) => this.email = event.target.value}/>
+            <input name="email" type="email" placeholder="Your Email Address" class="form-input" required/>
           </div>
           <div class="input-group">
             <h2>Message<span class="required">  *</span></h2>
-            <textarea 
+            <textarea
+              name="message"
               placeholder="Message"
               class="form-input"
               minlength="10"
-              onInput={(event: any) => this.message = event.target.value}/>
+              required/>
           </div>
+          <input type="hidden" name="_subject" value="Theracode Contact Us"/>
           <div class="input-group">
-            <ion-button type="submit" expand="block" disabled={!isActive}>Contact Us</ion-button>
+            <ion-button type="submit" expand="block">Contact Us</ion-button>
           </div>
         </form>
       </div>
@@ -47,18 +40,3 @@ export class ThContactBodyContent {
     ];
   }
 }
-
-
-/*
-<div class="input-group">
-            <div>
-              <label>Email Address<span class="required">*</span></label>
-            </div>
-            <input type="text" placeholder="Your Email"/>
-          </div>
-          <div class="input-group">
-            <div>
-              <label>Message<span class="required">*</span></label>
-            </div>
-            <textarea/>
-          </div>*/
--- src/components/footer/mailing-list/mailing-list.tsx
+++ src/components/footer/mailing-list/mailing-list.tsx
@@ -1,4 +1,4 @@
-import { Component, State } from '@stencil/core';
+import { Component } from '@stencil/core';
 
 @Component({
   tag: 'mailing-list',
@@ -6,53 +6,22 @@ import { Component, State } from '@stencil/core';
   
 })
 export class MailingList {
-  
-  @State() emailAddress: string;
-  @State() postResult: SuccessMessage;
-
-  async submitEmail() {
-    /*try {
-      await postEmailAddress(this.emailAddress);
-      this.postResult = {
-        success: true,
-        message: 'Thank you for subscribing'
-      }
-    } catch (ex) {
-      this.postResult = {
-        success: false,
-        message: 'Something went wrong. Please try again.'
-      }
-    }
-    setTimeout(() => {
-      this.postResult = null;
-    }, 500000);
-    */
-  }
 
   render() {
     return [
       <div class="container">
         <h1>Start Building Better Apps Today</h1>
         <p>Are you an enterprise building with Ionic? Do you want the best information and resources on how to ensure your project is successful? Sign up for our mailing list to have the very best Ionic in enterprise information sent directly to your inbox. We will never, under any circumstances, spam you or sell your info.</p>
-        <div class="email-container">
-          <ion-item>
-            <ion-input type="email" placeholder="Email Address" onInput={(event: any) => { 
-              this.emailAddress = event.target.value;
-              }}></ion-input>
-          </ion-item>
-          <ion-button onClick={ () => { this.submitEmail() }}>Subscribe</ion-button>
-        </div>
-        <div class="submit-text">{ this.postResult ? this.postResult.message : ''}</div>
+        <form action="https://formspree.io/dan.bucholtz@gmail.com" method="POST">
+          <div class="email-container">
+            <ion-item>
+              <ion-input name="email" type="email" placeholder="Email Address" required></ion-input>
+            </ion-item>
+            <input type="hidden" name="_subject" value="Theracode Mailing List"/>
+            <ion-button type="submit">Subscribe</ion-button>
+          </div>
+        </form>
       </div>
     ];
   }
-}
-
-export function postEmailAddress(_emailAddress: string) {
-  //return Promise.reject(new Error(''));
-}
-
-interface SuccessMessage {
-  success: boolean;
-  message: string;
 }
